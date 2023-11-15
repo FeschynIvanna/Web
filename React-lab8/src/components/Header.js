cf50188a818537630deca9db15b02208bc993aa8
FeschynIvanna <119298256+FeschynIvanna@users.noreply.github.com>
2023-11-15 16:29:35 +0200

    adding lab8

---
+++ React-lab8/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <nav>
+            <span className='logo'>Flower</span>
+            <ul className='nav'> 
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/catalog">Catalog</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
+            </ul>
+        </nav>
+        <div className='presentation'></div>
+    </header>
+)
+
+export default Header;
---
+++ React-lab8/src/components/Items.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const flowersData = [
+{
+    id: 1,
+    title: 'Ірис',
+    img: 'images2.jpg',
+    price: '49.99',
+    description: 'Дуже гарний ірис'
+},
+{
+    id: 2,
+    title: 'Соняшник',
+    img: 'images3.jpg',
+    price: '99.99',
+    description: 'Великий і яскравий соняшник'
+},
+{
+    id: 3,
+    title: 'Троянда',
+    img: 'images6.jpg',
+    price: '70.00',
+    description: 'Червона троянда з красивим запахом'
+},
+{
+    id: 4,
+    title: 'Тюльпан',
+    img: 'images1.jpg',
+    price: '45.50',
+    description: 'Ніжний тюльпан рожевого кольору'
+},
+{
+    id: 5,
+    title: 'Орхідея',
+    img: 'images5.jpg',
+    price: '88.60',
+    description: 'Невелика орхідея рожевого кольору'
+},
+{
+    id: 6,
+    title: 'Фіалка',
+    img: 'images4.jpg',
+    price: '30.00',
+    description: 'Ніжна фіалка'
+}
+];
+
+const Items = () => {
+const { flowerId } = useParams();
+const flower = flowersData.find((item) => item.id.toString() === flowerId);
+
+if (!flower) {
+    return <div>Flower not found!</div>;
+}
+
+return (
+    <div>
+    <h1>{flower.title}</h1>
+    <img src={flower.img} alt={flower.title} />
+    <p>Price: ${flower.price}</p>
+    <p>Description: {flower.description}</p>
+    </div>
+);
+}
+
+export default Items;
